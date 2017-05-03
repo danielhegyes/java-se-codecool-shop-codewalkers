6cc406744d39dad2645f165cb174120f7795e66f
TahinDani <tahin.daniel@gmail.com>
2017-05-03 10:15:34 +0200

    Order, Lineorder, TestOrder classes added

---
+++ src/main/java/com/codecool/shop/model/Lineitem.java
@@ -0,0 +1,46 @@
+package com.codecool.shop.model;
+
+
+
+/**
+ * Created by tahin on 2017.05.02..
+ */
+public class Lineitem {
+
+    private Product product;
+    private int quantity = 1;
+    private float linePrice;
+
+    public Lineitem(Product product){
+        this.product = product;
+        this.linePrice = product.getDefaultPrice();
+    }
+
+    public float getLinePrice() {
+        return linePrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Lineitem{" +
+                "product=" + product +
+                ", quantity=" + quantity +
+                ", linePrice=" + linePrice +
+                '}';
+    }
+
+    public void addOneToQuantity(){
+        this.quantity += 1;
+        this.linePrice += this.product.getDefaultPrice();
+    }
+
+    public void subOneFromQuantity(){
+
+        if (this.quantity == 1) {
+            this.quantity = 1;
+        } else {
+            this.quantity -= 1;
+            this.linePrice -= this.product.getDefaultPrice();
+        }
+    }
+}
---
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -0,0 +1,40 @@
+package com.codecool.shop.model;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Created by tahin on 2017.05.02..
+ */
+public class Order {
+
+    private static int currentId = 0;
+
+    private int id;
+    private Set<Lineitem> orderLines = new LinkedHashSet<>();
+    private float total = 0;
+
+
+    public Order(){
+        this.id = currentId;
+        currentId++;
+    }
+
+    public Set<Lineitem> getOrderLines() {
+        return orderLines;
+    }
+
+    public void addLine(Lineitem line){
+        this.orderLines.add(line);
+        this.total += line.getLinePrice();
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", orderLines=" + orderLines +
+                ", total=" + total +
+                '}';
+    }
+}
