2e2e0dc6d6582cbe5cad82d43a265c656479963e
TahinDani <tahin.daniel@gmail.com>
2017-05-16 11:32:43 +0200

    ProductDaoWithJdbc started

---
+++ src/main/sql/init_db.sql
@@ -0,0 +1,32 @@
+--DROP TABLE IF EXISTS product, product_category, supplier;
+
+CREATE TABLE product_category
+(
+  id INTEGER PRIMARY KEY,
+  name varchar(40),
+  description varchar(40),
+  depatment VARCHAR(40)
+);
+
+CREATE TABLE supplier
+(
+  id INTEGER PRIMARY KEY,
+  name varchar(40),
+  description varchar(40)
+);
+
+CREATE TABLE product
+(
+  id INTEGER PRIMARY KEY,
+  name varchar(40),
+  description varchar(40),
+  default_price DOUBLE PRECISION,
+  default_currency varchar(3),
+  product_category INTEGER REFERENCES product_category(id),
+  supplier INTEGER REFERENCES supplier(id)
+);
+
+
+
+
+
