07e2597505678a5c2a8a0c98eadb8fdfa4d3b2e9
TahinDani <tahin.daniel@gmail.com>
2017-05-16 13:21:53 +0200

    populate.sql started

---
+++ src/main/sql/populate.sql
@@ -0,0 +1,7 @@
+INSERT INTO product_category (ID,NAME,description,department)
+VALUES (1, 'Tablet' ,'Hardware', 'Hardware');
+
+INSERT INTO product_category (ID,NAME,description,department)
+VALUES (1, 'neve' ,'leiras', 'Hardware');
+
+
