57cb35f9d4a8122398b34db556086fb03d72a796
aafonya <tolnai.judit12@gmail.com>
2017-05-17 16:49:58 +0200

    tests added

---
+++ tests/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBCTest.java
@@ -0,0 +1,57 @@
+package com.codecool.shop.dao.implementation;
+
+
+import com.codecool.shop.model.ProductCategory;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+/**
+ * Created by judit on 17.05.17.
+ */
+class ProductCategoryDaoJDBCTest {
+
+    @Test
+    public void addToDbOrMem() {
+        ProductCategoryDaoJDBC productCategoryDaoJDBC = ProductCategoryDaoJDBC.getInstance();
+        ProductCategoryDaoMem productCategoryDaoMem = ProductCategoryDaoMem.getInstance();
+        ProductCategory vegetable = new ProductCategory(34, "vegetable", "grocery", "descriptipn");
+
+        productCategoryDaoJDBC.add(vegetable);
+        productCategoryDaoMem.add(vegetable);
+
+        ProductCategory vegetableFromDb = productCategoryDaoJDBC.find(vegetable.getId());
+        ProductCategory vegetableFromMem = productCategoryDaoMem.find(vegetable.getId());
+
+        assertEquals(vegetable, vegetableFromDb);
+        assertEquals(vegetable, vegetableFromMem);
+    }
+
+    @Test
+    public void removeFromDbOrMem() {
+
+        ProductCategoryDaoJDBC productCategoryDaoJDBC = ProductCategoryDaoJDBC.getInstance();
+        ProductCategoryDaoMem productCategoryDaoMem = ProductCategoryDaoMem.getInstance();
+        ProductCategory vegetable = new ProductCategory(34, "vegetable", "grocery", "descriptipn");
+
+        productCategoryDaoJDBC.add(vegetable);
+        productCategoryDaoMem.add(vegetable);
+
+        productCategoryDaoJDBC.remove(vegetable.getId());
+        productCategoryDaoMem.remove(vegetable.getId());
+
+        ProductCategory vegetableFromDb = productCategoryDaoJDBC.find(vegetable.getId());
+        ProductCategory vegetableFromMem = productCategoryDaoMem.find(vegetable.getId());
+
+        assertNull(productCategoryDaoJDBC.find(vegetable.getId()));
+        assertNull(productCategoryDaoMem.find(vegetable.getId()));
+
+    }
+
+
+
+
+
+
+}
